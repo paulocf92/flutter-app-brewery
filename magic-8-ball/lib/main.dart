4dc05fe6fa0c58896a30de30e0583d4a05d16366
Paulo Filho <pcf.feat@gmail.com>
2020-04-30 14:34:41 -0400

    chore: draft base layout with app bar (challenge 1)

--- magic-8-ball/lib/main.dart
+++ magic-8-ball/lib/main.dart
@@ -9,6 +9,13 @@ void main() => runApp(
 class BallPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: Colors.blue,
+      appBar: AppBar(
+        backgroundColor: Colors.blue.shade900,
+        title: Text('Ask Me Anything'),
+      ),
+      body: Container(),
+    );
   }
 }
